d46d663d1beb6af837ccc3159f892aae695c3efd
LucaRoescheisen <luca.roescheisen@gmail.com>
2026-02-11 20:06:34 +1100

    .

--- sim/tb.cpp
+++ sim/tb.cpp
@@ -9,11 +9,11 @@ int main(int argc, char **argv) {
   Vtop_verilator* dut = new Vtop_verilator;
   dut->clk = 0;
   dut->reset = 1;
-      for (int i = 0; i < 10; i++) {  // simulate 10 cycles
-        dut->clk = !dut->clk;        // toggle clock
-        dut->eval();                 // evaluate DUT
+      for (int i = 0; i < 10; i++) {
+        dut->clk = !dut->clk;
+        dut->eval();
 
-        if (i == 1) dut->reset = 0;  // deassert reset after first half-cycle
+        if (i == 1) dut->reset = 0;
 
         std::cout << "Cycle " << i << ", clk=" << (int)dut->clk << ", pc=" << (int)dut->pc_out << std::endl;
     }
